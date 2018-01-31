b0d9e2d7202db93c4c59cfa420370371f2473287
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-01-31 18:08:26 +0100

    Yaml parsing fixed: collections are now correctly converted. Fixes #13

--- parsyfiles/plugins_optional/support_for_yaml.py
+++ parsyfiles/plugins_optional/support_for_yaml.py
@@ -7,6 +7,7 @@ import yaml
 from parsyfiles.converting_core import AnyObject
 from parsyfiles.parsing_core import AnyParser, SingleFileParserFunction
 from parsyfiles.parsing_registries import ParserFinder, ConversionFinder
+from parsyfiles.plugins_base.support_for_collections import convert_collection_values_according_to_pep
 
 
 def read_object_from_yaml(desired_type: Type[Any], file_object: TextIOBase, logger: Logger,
@@ -26,6 +27,27 @@ def read_object_from_yaml(desired_type: Type[Any], file_object: TextIOBase, logg
     return yaml.load(file_object)
 
 
+def read_collection_from_yaml(desired_type: Type[Any], file_object: TextIOBase, logger: Logger,
+                              conversion_finder: ConversionFinder, fix_imports: bool = True, errors: str = 'strict',
+                              **kwargs) -> Any:
+    """
+    Parses a collection from a yaml file.
+
+    :param desired_type:
+    :param file_object:
+    :param logger:
+    :param fix_imports:
+    :param errors:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    res = yaml.load(file_object)
+
+    # convert if required
+    return convert_collection_values_according_to_pep(res, desired_type, conversion_finder, logger, **kwargs)
+
+
 def get_default_yaml_parsers(parser_finder: ParserFinder, conversion_finder: ConversionFinder) -> List[AnyParser]:
     """
     Utility method to return the default parsers able to parse an object from a file.
@@ -40,10 +62,11 @@ def get_default_yaml_parsers(parser_finder: ParserFinder, conversion_finder: Con
                                      supported_types={AnyObject},
                                      ),
             # yaml for collection objects
-            SingleFileParserFunction(parser_function=read_object_from_yaml,
+            SingleFileParserFunction(parser_function=read_collection_from_yaml,
                                      custom_name='read_collection_from_yaml',
                                      streaming_mode=True,
                                      supported_exts={'.yaml','.yml'},
                                      supported_types={Tuple, Dict, List, Set},
+                                     function_args={'conversion_finder': conversion_finder}
                                      )
     ]
